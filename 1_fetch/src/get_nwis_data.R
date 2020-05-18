bf023035f8ab5f0f8029a22aa0151d658d68ecaa
Corson-Dosch <hcorson-dosch@usgs.gov>
2020-05-18 16:52:58 -0500

    Make requested changes to workflow

--- 1_fetch/src/get_nwis_data.R
+++ 1_fetch/src/get_nwis_data.R
@@ -1,28 +1,29 @@
-
-download_nwis_site_data <- function(site_nums = c("01427207", "01432160", "01435000", "01436690", "01466500", "01477050"), parameterCd = '00010', startDate="2014-05-01", endDate="2015-05-01"){
+generate_files <- function(site_nums = c("01427207", "01432160", "01435000", "01436690", "01466500", "01477050")){
   # tempdir() creates a temporary directory that is wiped out when you start a new R session; 
   # replace tempdir() with "1_fetch/out" or another desired folder if you want to retain the download
   download_files <- file.path("1_fetch/out", paste0('nwis_', site_nums, '_data.csv'))
-
-  for (filepath in download_files) {
-    # filepaths look something like directory/nwis_01432160_data.csv,
-    # remove the directory with basename() and extract the 01432160 with the regular expression match
-    site_num <- basename(filepath) %>% 
-      stringr::str_extract(pattern = "(?:[0-9]+)")
-    
-    # readNWISdata is from the dataRetrieval package
-    data_out <- readNWISdata(sites=site_num, service="iv", 
-                             parameterCd = parameterCd, startDate = startDate, endDate = endDate)
-    
-    # -- simulating a failure-prone web-sevice here, do not edit --
-    if (sample(c(T,F,F,F), 1)){
-      stop(site_num, ' has failed due to connection timeout. Try scmake() again')
-    }
-    # -- end of do-not-edit block
-    
-    write_csv(data_out, path = filepath)
-  }
+  
   return(download_files)
+}
+
+
+download_nwis_site_data <- function(filepath, parameterCd = '00010', startDate="2014-05-01", endDate="2015-05-01"){
+  # filepaths look something like directory/nwis_01432160_data.csv,
+  # remove the directory with basename() and extract the 01432160 with the regular expression match
+  site_num <- basename(filepath) %>% 
+    stringr::str_extract(pattern = "(?:[0-9]+)")
+  
+  # readNWISdata is from the dataRetrieval package
+  data_out <- readNWISdata(sites=site_num, service="iv", 
+                           parameterCd = parameterCd, startDate = startDate, endDate = endDate)
+  
+  # -- simulating a failure-prone web-sevice here, do not edit --
+  if (sample(c(T,F,F,F), 1)){
+    stop(site_num, ' has failed due to connection timeout. Try scmake() again')
+  }
+  # -- end of do-not-edit block
+  
+  write_csv(data_out, path = filepath)
 }
 
 import_nwis_data <- function(download_files) {
